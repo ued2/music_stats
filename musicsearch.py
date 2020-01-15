594e4cf238f230f4e7b4cbca9463c5199fbb87ea
Ugo Dike <ued2@nau.edu>
2020-01-15 00:16:06 -0700

    changed variable name

--- musicsearch.py
+++ musicsearch.py
@@ -6,15 +6,15 @@ name = input('Enter Artist: ')
 
 
 if platform == 'A':
-    Platform = pandas.read_csv('AppleMusic.csv',  delimiter=',', names = ['rank', 'artist', 'song'])
+    data = pandas.read_csv('AppleMusic.csv',  delimiter=',', names = ['rank', 'artist', 'song'])
 elif platform == 'B':
-    Platform = pandas.read_csv('BillboardTop100.csv',  delimiter=',', names = ['rank', 'artist', 'song'])
+    data = pandas.read_csv('BillboardTop100.csv',  delimiter=',', names = ['rank', 'artist', 'song'])
 elif platform == 'S':
-    Platform = pandas.read_csv('Spotify.csv',  delimiter=',', names = ['rank', 'artist', 'song', 'stream'])
+    data = pandas.read_csv('Spotify.csv',  delimiter=',', names = ['rank', 'artist', 'song', 'stream'])
 
 
 def look(name,platform):
-    print(Platform[Platform.artist == str(name.strip())])
-    print(Platform[Platform.artist == str(name.strip())][['song']].count())
+    print(data[data.artist == str(name.strip())])
+    print(data[data.artist == str(name.strip())][['song']].count())
 
 look(name,platform)
